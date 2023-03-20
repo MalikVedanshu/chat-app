6fc23017ef1b1fe1a77c7a06383f14cc1704561a
MalikVedanshu <malikvedanshu757@outlook.com>
2023-03-20 15:41:41 +0000

    try1

--- server/app.js
+++ server/app.js
@@ -1,13 +1,14 @@
 import express from 'express';
-const app = express();
 import http from "http";
 import { Server } from 'socket.io';
-// const path = require('path');
+import path from 'path';
+import { fileURLToPath } from 'url';
 // const cors = require("cors");
 
 let port = 5000;
-
-// app.use(express.static(path.join(__dirname, 'build')));
+const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+app.use(express.static(path.join(__dirname, 'build')));
 
 // app.use(cors())
 
@@ -23,29 +24,24 @@ const io = new Server(server);
 // });
 
 io.on('connection', socket => {
-    
     socket.on("send-sms", (msg, room,name) => {
         if(name === "") {
             socket.to(room).emit("recieve-sms", [msg, "someone"]);
         } else {
             socket.to(room).emit("recieve-sms", [msg, name]);
         }
-        
     })
-    socket.on('join-room', (room,name )=> {
+    socket.on('join-room', (room, name)=> {
         socket.join(room);
         if(name === "") {
             socket.to(room).emit("someone-joined", "Someone");
         } else {
             socket.to(room).emit("someone-joined", name);
         }
-        
     })
 })
 
 
 server.listen(port, () => {
-    console.log(`Server is running on port ${port}.`)
+    console.log(`Server is running on port ${port}.`);
 })
-
-
